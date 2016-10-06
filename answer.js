35cf1d266f2ab40071f375b0736143e100271742
Charles <charles.jambon@gmail.com>
2016-10-06 19:22:47 +0000

    struggling with 2B and 3

--- answer.js
+++ answer.js
@@ -1,5 +1,5 @@
-
-// Write a function called printPositives that takes an array and uses the forEach method to print only the positive numbers.
+// Write a function called printPositives that takes an array and uses the 
+//forEach method to print only the positive numbers.
 
 var positiveAndNegativeNumbers = [2, -4, 3, -5, 100, -Infinity];
 
@@ -13,14 +13,8 @@ function printPositives(array) {
     return n;
 };
 
-/*function x (arr){
-    var n = []
-    arr.for
-    return n
-}*/
-
 var onlyPositiveEach = printPositives(positiveAndNegativeNumbers);
-console.log(onlyPositiveEach);
+// console.log(onlyPositiveEach);
 
 // Similar to the previous exercise, write a function called getPositives that takes an array 
 // and uses the filter method to return a new array with only the positive numbers.
@@ -30,4 +24,24 @@ function getPositives(number) {
 }
 
 var onlyPositiveFilter = positiveAndNegativeNumbers.filter(getPositives);
-console.log(onlyPositiveFilter);
+// console.log(onlyPositiveFilter);
+
+// Re-do exercise 1 by first filtering the input array, and then printing the numbers from the filtered array.
+// Your code will look something like: return arr.filter(...).forEach(...).
+
+
+
+/* Write a function called filterArray that takes a callback function and an array as arguments. 
+Your filterArray function should return a new array that contains only the elements where the callback 
+function returns true.
+NOTE: This is a trick question. The answer is a one-liner and you are allowed to use array.filter :)*/
+
+var trickQuestion = [true, 0, false, 1];
+
+function filterArray(number) {
+	return number == true;
+}
+
+var answerTrickQuestion = trickQuestion.filter(filterArray);
+console.log(answerTrickQuestion);
+
